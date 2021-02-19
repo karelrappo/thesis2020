59b48dfbd6894f5da5237ec6e0b12484210a0f7e
karelrappo <karerappo@gmail.com>
2021-02-19 10:29:14 +0200

    updates

--- functions.R
+++ functions.R
@@ -72,7 +72,7 @@ out_of_sample <- function(var,var2,type)
 }
 
 
-##############################   FORMAT RESULTS  ##############################################################
+##############################   FORMAT OOS RESULTS  ##############################################################
 
 
 df_results1 <- as.data.frame(t(mapply(out_of_sample, colnames(df)[29:40], "YIV", "full")))
@@ -107,50 +107,6 @@ rownames(df_resultss)[rownames(df_resultss)=='YIV_Recessionary'] <- "YIV-Recess.
 rownames(df_resultss)[rownames(df_resultss)=='YIV_Expansionary'] <- "YIV-Expans."
 rownames(df_resultss)[rownames(df_resultss)=='Naive_Recessionary'] <- "Naive-Recess."
 rownames(df_resultss)[rownames(df_resultss)=='Naive_Expansionary'] <- "Naive-Expans."
-
-
-#selle vist võib ära cuttida, siis kui plotid on oiged pandud appendicesse?
-###############################################################################################
-########################   Random Forest YIV ONLY   ###########################################
-###############################################################################################
-
-out_of_samp <- function(var1, var2, var3){
-  mycontrol <- trainControl(method = "timeslice",
-                            initialWindow = 20,
-                            horizon = 1,
-                            fixedWindow = TRUE, 
-                            savePredictions = TRUE)
-  myfit <- train(as.formula(paste0(var1, "~", var2)), data = df[1:sum(!is.na(df[var1])),],
-                 method = "rf",
-                 ntree = 50,
-                 trControl = mycontrol)
-  dff <- myfit$pred
-  SE <- (dff$pred-dff$obs)^2
-  if (var3 == 5){
-    SE <- SE[c(25,26,27,52,53,54,55,56,57)]
-  }
-  if (var3 == 6){
-    SE <- SE[-c(25,26,27,52,53,54,55,56,57)]
-  }
-  RMSFE <- sqrt(mean(SE))
-  return(RMSFE)
-} 
-
-
-rf_results1 <- as.data.frame(t(mapply(out_of_samp, colnames(df)[29:40], "YIV", 4)))
-rf_results2 <- as.data.frame(t(mapply(out_of_samp, colnames(df)[29:40], "YIV", 5)))
-rf_results3 <- as.data.frame(t(mapply(out_of_samp, colnames(df)[29:40], "YIV", 6)))
-
-
-
-rf_resultss <- rbind(rf_results1, rf_results2, rf_results3)
-
-rf_resultss <- rf_resultss %>%
-  round(2)
-
-colnames(rf_resultss, do.NULL = FALSE)
-colnames(rf_resultss) <- c("H1","H2","H3","H4","H5","H6","H7","H8","H9","H10","H11","H12")
-rownames(rf_resultss) <- c("RF_YIV", "RF_Recessionary", "RF_Expansionary")
 
 
 ###############################################################################################
